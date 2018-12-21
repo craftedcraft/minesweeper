73f908d2f0180274f5f8d82feb25a552dfc6f6cf
RMH <riki@craftsmoothie.co.nz>
2018-12-21 21:06:08 +1300

    Minesweeper

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,50 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+var board = {
+  cells: generateCells(3)
+}
+
+function generateCells (size) {
+
+var cells = []
+
+for (i = 0; i < size; i++) {
+  for (j = 0; j < size; j++) {
+
+      cell = {
+        row: i,
+        col: j,
+        isMine: Math.floor(Math.random()*1.5),
+        isMarked: false,
+        hidden: true
+      }
+
+      cells.push(cell)
+    }
+  }
+
+  return cells
+}
 
 function startGame () {
-  // Don't remove this function call: it makes the game work!
+
+// board.cells[4].isMine = true
+// board.cells[7].isMine = true
+// board.cells[1].isMine = true
+// board.cells[22].isMine = true
+// board.cells[9].isMine = true
+// board.cells[8].isMine = true
+
   lib.initBoard()
+
+  for (i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  }
+
+  document.addEventListener('click', checkForWin); 
+  document.addEventListener('contextmenu', checkForWin);
+
 }
 
 // Define this function to look for a win condition:
@@ -14,9 +53,25 @@ function startGame () {
 // 2. Are all of the mines marked?
 function checkForWin () {
 
+  // loop through array of cells
+  // check for isMinne.False is visable
+  // check mines are hidden
+
+  for (var i = 0; i < board.cells.length; i++) {
+
+    var checkWinner = board.cells[i]
+    if(checkWinner.isMine && checkWinner.isMarked) {
+      return;
+
+     }
+     else if(!checkWinner.isMine && checkWinner.hidden){
+       return;
+    }
+  };
+
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+    lib.displayMessage('How Good?!')
 }
 
 // Define this function to count the number of mines around the cell
@@ -28,5 +83,16 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+  var count = 0
+
+  for (j = 0; j < surrounding.length; j++) {
+    if (surrounding[j].isMine) {
+      count++
+    }
+  }
+
+  return count
 }
 
